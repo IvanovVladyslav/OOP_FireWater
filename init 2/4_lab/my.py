f2e88fa6f8e3c0060c179301c0b74812a494f231
Ivanov Vladislav <vivanov2021@itcollege.lviv.ua>
2023-11-22 09:33:01 +0200

    forprelastlab

--- init 2/4_lab/my.py
+++ init 2/4_lab/my.py
@@ -1,7 +1,15 @@
-import requests
+#import requests
 
-r = requests.get('https://httpbin.org/')
-print(r.encoding)
+#r = requests.get('https://httpbin.org/')
+#print(r.encoding)
 
-for line in r.iter_lines():
-    print(line)
+#for line in r.iter_lines():
+    #print(line)
+
+
+from jikanpy import Jikan
+jikan = Jikan()
+
+anime = jikan.anime(52991, extension='episodes')
+for episode in anime['data']:
+    print(f"Епізод №{episode['mal_id']} з назвою: {episode['title']} має рейтинг {episode['score']}")
